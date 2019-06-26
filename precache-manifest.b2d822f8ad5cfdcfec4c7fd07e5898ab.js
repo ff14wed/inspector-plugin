9dcdbd064a5c56e3f55036ea7b5851aab3962736
Flawed <33593723+ff14wed@users.noreply.github.com>
2019-06-26 04:04:34 -0700

    Add build of Inspector

---
+++ precache-manifest.b2d822f8ad5cfdcfec4c7fd07e5898ab.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "103417c2cd99deff6e7360eb06aca64e",
+    "url": "/inspector-plugin/index.html"
+  },
+  {
+    "revision": "36cfebe582c8570fcc6a",
+    "url": "/inspector-plugin/static/css/2.74c780e0.chunk.css"
+  },
+  {
+    "revision": "4f28c5ef490629e4058b",
+    "url": "/inspector-plugin/static/css/main.1bd4c675.chunk.css"
+  },
+  {
+    "revision": "36cfebe582c8570fcc6a",
+    "url": "/inspector-plugin/static/js/2.1b7507ac.chunk.js"
+  },
+  {
+    "revision": "4f28c5ef490629e4058b",
+    "url": "/inspector-plugin/static/js/main.54166a9a.chunk.js"
+  },
+  {
+    "revision": "275fedd9d1f0cbd4f680",
+    "url": "/inspector-plugin/static/js/runtime~main.4df81f35.js"
+  },
+  {
+    "revision": "5fa1b8f25b4aa8787f70d5b6b7f0b90f",
+    "url": "/inspector-plugin/static/media/icons-16.5fa1b8f2.ttf"
+  },
+  {
+    "revision": "6fdb722c94434811f89a68eafc4531f8",
+    "url": "/inspector-plugin/static/media/icons-16.6fdb722c.eot"
+  },
+  {
+    "revision": "cd1a26696ebf17a89545a3f9067d7028",
+    "url": "/inspector-plugin/static/media/icons-16.cd1a2669.woff"
+  },
+  {
+    "revision": "14279fa7b78ac2230a015925b603a1f7",
+    "url": "/inspector-plugin/static/media/icons-20.14279fa7.eot"
+  },
+  {
+    "revision": "657723dc996cf0ae6b4c6110f5d4ddab",
+    "url": "/inspector-plugin/static/media/icons-20.657723dc.woff"
+  },
+  {
+    "revision": "90247cfdde1bd7b76d804a797d5dec56",
+    "url": "/inspector-plugin/static/media/icons-20.90247cfd.ttf"
+  }
+]);
